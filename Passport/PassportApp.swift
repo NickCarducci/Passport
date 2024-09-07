491e7ade18b6a81a1533611de401b7f13a9186f2
Nicholas Carducci <nmcarducci@gmail.com>
2024-09-07 10:45:17 -0400

    Initial Commit

---
+++ Passport/PassportApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassportApp.swift
+//  Passport
+//
+//  Created by Nicholas Carducci on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PassportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
