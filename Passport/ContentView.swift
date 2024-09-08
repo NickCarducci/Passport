44aa6d3323fcf70cf10092e95634540214df0716
Nicholas Carducci <nmcarducci@gmail.com>
2024-09-08 07:37:45 -0400

    ok

--- Passport/ContentView.swift
+++ Passport/ContentView.swift
@@ -4,20 +4,154 @@
 //
 //  Created by Nicholas Carducci on 9/7/24.
 //
-
+import Foundation
+import PromiseKit
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+//import FirebaseMessaging
 
+enum FirebaseError: Error {
+    case Error
+    case VerificatrionEmpty
+}
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State var verificationCode = ""
+    @State var verificationID = ""
+    @State var phoneNumber = ""
+    @State var countryCodeNumber = "+1"
+    @State var country = ""
+    @State var smsTextCode = ""
+    @State var timerExpired = false
+    @State var timeStr = ""
+    @State var timeRemaining = 60
+    @State var loggedin = false
+    @State var verificationId = ""
+    @State var verifiable = false
+    @State var testing = false
+    @GestureState private var isTapped = false
+    //@State var session: User = User(coder: NSCoder())!
+    
+    init() {
+        // Use Firebase library to configure APIs
+        //FirebaseApp.configure()
+        
+    }
+
+    func signOut(){
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+            }
+            catch {
+              print (error)
+            }
         }
-        .padding()
+    
+        loggedin = false
+    }
+    
+    var body: some View {
+        if Auth.auth().currentUser == nil {
+            Form{
+                Section(footer: Text("Get an SMS code. Standard messaging rates apply.")) {
+                    TextField("Country code", text: $countryCodeNumber)
+                        .font(Font.system(size: 15))
+                        .fontWeight(.semibold)
+                        .frame(width: nil, height: nil, alignment: .leading)
+                        .onChange(of: countryCodeNumber) {
+                          verifiable = false
+                        }
+                    TextField("Enter your phone number", text: $phoneNumber)
+                        .font(Font.system(size: 15))
+                        .fontWeight(.semibold)
+                        .frame(width: nil, height: nil, alignment: .leading)
+                        .onChange(of: phoneNumber) {
+                          verifiable = false
+                        }
+                }
+                if verifiable {
+                    Section{
+                        TextField("SMS code", text: $verificationCode)
+                            .font(Font.system(size: 15))
+                            .fontWeight(.semibold)
+                            .frame(width: nil, height: nil, alignment: .leading)
+                    }
+                }
+                Button("Submit", action: {
+                    
+                    if !verifiable {
+                        /*let _ = Auth.auth().addStateDidChangeListener({ (auth, user) in
+                            if let _ = user {
+                                loggedin = true
+                                //session = user
+                            } else {
+                                loggedin = false
+                                //session = User(coder: NSCoder())!
+                            }
+                        })*/
+                        
+                        //testing = true
+                        //Auth.auth().settings?.isAppVerificationDisabledForTesting = true
+                        let _ = PhoneAuthProvider.provider(auth: Auth.auth())
+                        PhoneAuthProvider.provider().verifyPhoneNumber(
+                            countryCodeNumber + phoneNumber, uiDelegate: nil) { verificationID, error in
+                            if error != nil {
+                                print(error!.localizedDescription)
+                                return
+                            }
+                            guard let verificationID = verificationID else {
+                                print(FirebaseError.VerificatrionEmpty)
+                                return
+                            }
+                            verificationId = verificationID
+                            verifiable = true
+                        }
+                        /*firstly {
+                            signUp(phoneNumber: countryCodeNumber + phoneNumber)
+                        }.done(on: DispatchQueue.main) { id in
+                            verificationId = id
+                        }.catch { (error) in
+                            print(error.localizedDescription)
+                        }*/
+                    } else {
+                        let credential = PhoneAuthProvider.provider().credential(
+                          withVerificationID: verificationId,
+                          verificationCode: smsTextCode
+                        )
+                        Auth.auth().signIn(with: credential) { authResult, error in
+                            if error != nil {
+                                print(FirebaseError.Error)
+                                return
+                            }
+                            guard let authResult = authResult else {
+                                print(FirebaseError.VerificatrionEmpty)
+                                return
+                            }
+                            _ = authResult
+                        }
+                    }
+                    
+                    
+                })
+            }
+        }
+        if(testing){
+            Text(countryCodeNumber + phoneNumber)
+        }
+        if Auth.auth().currentUser != nil {
+            Text("logout")
+                .font(Font.system(size: 15))
+                .fontWeight(.semibold)
+                .frame(width: nil, height: nil, alignment: .leading)
+                .onTapGesture {
+                    signOut()
+                }
+        }
     }
 }
+
 
 #Preview {
     ContentView()
--- Passport/PassportApp.swift
+++ Passport/PassportApp.swift
@@ -6,12 +6,93 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+//import FirebaseMessaging
 
 @main
 struct PassportApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+/*class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  
+  func application(_ application: UIApplication, open url: URL, 
+                   options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    print("\(#function)")
+    if Auth.auth().canHandle(url) {
+      return true
+    }
+    return false
+  }
+}*/
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification notification: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("\(#function)")
+        if Auth.auth().canHandleNotification(notification) {
+            completionHandler(.noData)
+            return
+        }
+    }
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        /*return UISceneConfiguration(
+          name: "Default Configuration",
+          sessionRole: connectingSceneSession.role
+        )*/
+          let sceneConfig: UISceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+          sceneConfig.delegateClass = SceneDelegate.self
+          return sceneConfig
+      }
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      print("\(#function)")
+      if Auth.auth().canHandle(url) {
+        return true
+      }
+      return false
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("Yay! Got a device token 🥳 \(deviceToken)")
+        print("\(#function)")
+        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+        //Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  //var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+      //guard let windowScene = (scene as? UIWindowScene) else { return }
+  }
+
+  // Implementing this delegate method is needed when swizzling is disabled.
+  // Without it, reCAPTCHA's login view controller will not dismiss.
+  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    for urlContext in URLContexts {
+      let url = urlContext.url
+      _ = Auth.auth().canHandle(url)
+    }
+
+    // URL not auth related; it should be handled separately.
+  }
+
+
 }
